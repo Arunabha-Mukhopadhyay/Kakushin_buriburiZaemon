ccffd06343ba1dfeb1b3f3d9262632c1d5b715db
Arunabha-Mukhopadhyay <dkarunabha2006@gmail.com>
2026-07-28 11:17:13 +0530

    added the prisma schema for the project requirements

---
+++ backend/server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
